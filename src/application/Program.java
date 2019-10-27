c9d896a37ad86668e5b5b5c8d56d90f3973077aa
Sérgio Developer <sergiodeveloperlc@protonmail.com>
2019-10-27 08:23:35 -0300

    Azul caneta

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.print("Bom dia!");
 		System.out.println("Caneta Azul");
-
+		System.out.println("Azul Caneta");
 	}
-
 }
